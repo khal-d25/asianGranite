49a7ff8b1758ebbf8c928ee0d7584a8299b23ee4
khal-d25 <54522993+khal-d25@users.noreply.github.com>
2021-07-04 19:58:38 +0530

    gell

--- gallery.js
+++ gallery.js
@@ -14,4 +14,61 @@ cross.addEventListener('click',()=>{
     menu.style.opacity = '100%'
 
 })
+
+const names = ['Rajasthan Stone1','Rajasthan Stone2','Rajasthan Stone3','Rajasthan Stone4','Rajasthan Stone5','Rajasthan Stone6','Rajasthan Stone7','Rajasthan Stone8','Rajasthan Stone9']
+const desc = ['About of 1 BLOCKS: Maximum size of 8ft x 5ft x 5ft SLABS: Maximum size of 8ft x 5ft with thickness of 2-3cm','About of 2','About of 3','About of 4','About of 5','About of 6','About of 7','About of 8','About of 9']
+
+catimgs = document.querySelectorAll('.catimg')
+
+catimgs.forEach(n => {
+    n.addEventListener('click',()=>{
+        // console.dir(n)
+        const torem = document.querySelector('.show')
+        console.log(torem)
+        if(torem !== null){
+            torem.remove()
+        }
+        // torem.remove()
+        const a = document.createElement('div')
+        const b = document.createElement('div')
+        const c = document.createElement('div')
+        const d = document.createElement('h1')
+        const e = document.createElement('p')
+        const f = document.createElement('img')
+        const g = document.createElement('p')
+        
+        a.classList.add('show')
+        b.classList.add('div1')
+        c.classList.add('div2')
+        const value = n.nextElementSibling.innerText;
+        console.log(value)
+        d.innerText = value;
+        e.innerHTML = '<i class="uil uil-multiply"></i>'
+        
+        b.append(d)
+        b.append(e)
+        
+        f.src = n.src;
+        const i = names.indexOf(value)
+        g.innerText = desc[i]
+
+        c.append(f)
+        c.append(g)
+
+        a.append(b)
+        a.append(c)
+
+        e.addEventListener('click',()=>{
+            e.parentElement.parentElement.remove()
+        })
+
+        document.body.append(a)
+
+
+
+
+        
+    })
+    
+});
     
